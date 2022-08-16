4096b3f045b6699fa7c645fd2a161da4bd22aca2
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-16 09:06:54 +0200

    Added an example using the scheduler

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 // - Libraries
 #include <Arduino.h>
 #include <WiFiNINA.h>
+#include <Scheduler.h>
 // - Custom classes
 #include "WiFi/LPOSWiFi.h"
 #include "Serial/LPOSSerial.h"
@@ -18,6 +19,33 @@ int status = WL_IDLE_STATUS;	// Setting the status of the Wi-Fi to "idle" as def
 WiFiClient wiFiClient;			// Create the Wi-Fi client
 // - Millis
 unsigned long lastMillis = 0;
+const int LED = 13;
+
+void setup2()
+{
+	Serial.print(millis());
+	Serial.println(F(":setup2"));
+	pinMode(LED, OUTPUT);
+}
+
+void loop2()
+{
+	// Turn LED off
+	Serial.print(millis());
+	Serial.println(F(":loop2::led off"));
+	digitalWrite(LED, LOW);
+	delay(1000);
+
+	// Turn LED on
+	Serial.print(millis());
+	Serial.println(F(":loop2::led on"));
+	digitalWrite(LED, HIGH);
+	delay(1000);
+
+	Serial.print(millis());
+	Serial.print(F(":loop2::stack="));
+	Serial.println(SchedulerClass::stack());
+}
 
 /**
  * Initialises the program
@@ -35,9 +63,11 @@ void setup() {
 	LPOSWiFi::WiFiStartup((char *) SECRET_SSID, (char *) SECRET_PASS, status);
 	// Say what SSID we connected to, and what IP we got.
 	LPOSWiFi::PrintWiFiStatus();
+
+	SchedulerClass::start(setup2, loop2);
 }
 
-/***
+/**
  * The code to be executed
  */
 void loop() {
