23eb4260b72ec67dfcafd5cc35329409ec1fe6d3
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-22 16:38:47 +0200

    Added new example and manged to get BPM

--- examples/heartrate.cpp
+++ examples/heartrate.cpp
@@ -1,3 +1,55 @@
-//
-// Created by angus on 8/22/22.
-//
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+bool peaked = false;
+bool caved = true;
+int peakMinimum = 850;
+int peakCount = 0;
+int recordTime = 5;
+unsigned long lastMillis = millis();
+int bpm = 0;
+int ledState = LOW;
+
+int getBPM () {
+
+	int sensorValue = analogRead(A1);
+	if (sensorValue >= peakMinimum) {
+		if (caved) {
+			peaked = true;
+			caved = false;
+			ledState = HIGH;
+			peakCount++;
+		}
+	} else if (sensorValue < peakMinimum) {
+		if (peaked) {
+			peaked = false;
+			caved = true;
+			ledState = LOW;
+		}
+	}
+
+	if ((millis() - lastMillis) >= (recordTime * 1000)) {
+		bpm = 60*(peakCount)/recordTime;
+		peakCount = 0;
+		lastMillis = millis();
+	}
+
+	digitalWrite(LED_BUILTIN, ledState);
+
+	return bpm;
+}
+
+void setup () {
+	// initialize serial communication at 9600 bits per second:
+	Serial.begin(9600);
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop () {
+
+	Serial.print("BPM: ");
+	Serial.println(getBPM());
+
+	delay(50);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,55 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
-#include "DFRobot_Heartrate.h"
-DFRobot_Heartrate heartrate(DIGITAL_MODE);
-
-unsigned long thisMillis = 0;
-unsigned long lastMillis = 0;
-
+bool peaked = false;
+bool caved = true;
 int peakMinimum = 850;
+int peakCount = 0;
+int recordTime = 5;
+unsigned long lastMillis = millis();
+int bpm = 0;
+int ledState = LOW;
 
-unsigned long time;
+int getBPM () {
 
+	int sensorValue = analogRead(A1);
+	if (sensorValue >= peakMinimum) {
+		if (caved) {
+			peaked = true;
+			caved = false;
+			ledState = HIGH;
+			peakCount++;
+		}
+	} else if (sensorValue < peakMinimum) {
+		if (peaked) {
+			peaked = false;
+			caved = true;
+			ledState = LOW;
+		}
+	}
 
-void setup() {
+	if ((millis() - lastMillis) >= (recordTime * 1000)) {
+		bpm = 60*(peakCount)/recordTime;
+		peakCount = 0;
+		lastMillis = millis();
+	}
+
+	digitalWrite(LED_BUILTIN, ledState);
+
+	return bpm;
+}
+
+void setup () {
 	// initialize serial communication at 9600 bits per second:
 	Serial.begin(9600);
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
-void loop() {
-	heartrate.getValue(PIN_PA22);
-	int sensorValue = analogRead(A1);
-	if(sensorValue >= peakMinimum) {
-		Serial.println(heartrate.getRate());
-	}
-	delay(250);
+void loop () {
+
+	Serial.print("BPM: ");
+	Serial.println(getBPM());
+
+	delay(50);
 }
