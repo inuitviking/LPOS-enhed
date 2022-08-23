30d98e5568aeed6c4b5a335ee2887b9f1a18a683
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-23 10:01:14 +0200

    .idea stuff

---
+++ lib/classes/Heartbeat.cpp
@@ -0,0 +1,5 @@
+//
+// Created by angus on 8/23/22.
+//
+
+#include "Heartbeat.h"
---
+++ lib/classes/Heartbeat.h
@@ -0,0 +1,14 @@
+//
+// Created by angus on 8/23/22.
+//
+
+#ifndef LPOS_ENHED_HEARTBEAT_H
+#define LPOS_ENHED_HEARTBEAT_H
+
+
+class Heartbeat {
+
+};
+
+
+#endif //LPOS_ENHED_HEARTBEAT_H
