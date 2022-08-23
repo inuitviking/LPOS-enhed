27fa7c39a45d3267625c31fc02a2bb8955ffa017
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-23 12:55:06 +0200

    Added libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 // Official libraries
-#include <Arduino.h>	// Require to easier talk with the Arduino
-#include <SPI.h>		// Required by the ECCX08CSR library.
-#include <Wire.h>		// Required by the ECCX08CSR library.
+#include <Arduino.h>		// Require to easier talk with the Arduino
+#include <SPI.h>			// Required by the ECCX08CSR library.
+#include <Wire.h>			// Required by the ECCX08CSR library.
+#include <WiFiNINA.h>		// Required to connect to wifi
+#include <SSLClient.h>		// https://github.com/OPEnSLab-OSU/SSLClient
+#include <PubSubClient.h>	// https://pubsubclient.knolleary.net/
 
 // Own libraries
-#include "Heartbeat.h"	// Library for listening to the pulse sensor
-#include "Serial/LPOSSerial.h"
+#include "Heartbeat.h"			// Library for listening to the pulse sensor
+#include "Serial/LPOSSerial.h"	// Library for clearing the serial output (only works in putty)
+
+// Macros
+#include "Secrets/certificates.h"	// Where we store certificates
+#include "Secrets/wifi.h"			// Where we store WiFi credentials
+#include "Secrets/mqtt.h"			// Where we store MQTT credentials
 
 // Variables
 Heartbeat heartbeat;
